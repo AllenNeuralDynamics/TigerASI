f97a17a8ec05b9011f5b51f99ac81569b8185caa
Joshua Vasquez <joshua.vasquez@alleninstitute.org>
2022-09-23 11:27:02 -0700

    adding missing CNTS device code.

--- tigerasi/device_codes.py
+++ tigerasi/device_codes.py
@@ -14,6 +14,7 @@ class Cmds:
     HERE = b"H"  # [axis]=0 [axis]=0
     WHERE = b"W"  # [axis] [axis]
     BACKLASH = b"B" # [axis]=0 [axis]=0
+    CNTS = b"CNTS" # [axis]?
 
 
 class ErrorCodes(Enum):
