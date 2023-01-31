09cd0ecacab0a894413ef2970143eb2b89ec55d2
Joshua Vasquez <joshua.vasquez@alleninstitute.org>
2023-01-30 17:46:23 -0800

    update doc template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,8 +7,8 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'TigerASI'
-copyright = '2023, poofjunior, adamkglaser'
-author = 'poofjunior, adamkglaser'
+copyright = "2022, Allen Institute of Neural Dynamics"
+author = "Allen Institute of Neural Dynamics"
 release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
@@ -22,26 +22,36 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
 #html_theme = 'classic'
 #html_theme = 'alabaster'
-html_theme_options = {
-    #'page_width': 'auto',  # alabaseter theme spec
-    #'fixed_sidebar': True,  # alabaster theme spec
-    #'sidebar_width': '320px',  # alabaster theme spec
-#    'body_min_width': '70%', # classic theme spec
-#    'sidebarwidth': '320px', # classic theme spec
-}
+#html_theme_options = {
+#    #'page_width': 'auto',  # alabaseter theme spec
+#    #'fixed_sidebar': True,  # alabaster theme spec
+#    #'sidebar_width': '320px',  # alabaster theme spec
+##    'body_min_width': '70%', # classic theme spec
+##    'sidebarwidth': '320px', # classic theme spec
+#}
 # 'classic' theme sidebar settings.
 #html_sidebars = {'**': ['localtoc.html', 'sourcelink.html',
 #                        'searchbox.html', 'globaltoc.html']}
-html_static_path = []  #['_static']
+html_static_path = ['_static']
+html_favicon = "_static/favicon.ico"
+html_theme_options = {
+    "light_logo": "light-logo.svg",
+    "dark_logo": "dark-logo.svg",
+}
+
 
 # Sphinx Options.
 autoclass_content = 'both'
 toc_object_entries_show_parents = 'hide' # hide the full method path.
+
+# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
+html_show_sphinx = False
+
+# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
+html_show_copyright = False
